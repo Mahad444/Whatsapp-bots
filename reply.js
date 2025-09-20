b6d48c04a5b35fa7ae5c59099ea810e150b2e626
Ally Juma Said <mahadsaid95@icloud.com>
2025-09-19 23:23:45 -1100

    refactor: organize imports and add session file path comment

--- reply.js
+++ reply.js
@@ -1,3 +1,7 @@
 // reply from main.js
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const fs = require('fs');
+const path = require('path');
+
+// Path to the session file
